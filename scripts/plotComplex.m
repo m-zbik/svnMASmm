59163804c0f8c7c617d6c35a893e7432b83f00d0
lyran <lyran@8f7cbc4e-d821-48fb-b665-6de9fca9247a>
2008-05-08 15:34:03 +0000

    Added Hurst exponent exploration for Cont to the paper.

--- scripts/plotComplex.m
+++ scripts/plotComplex.m
@@ -1,51 +1,55 @@
 filename='..\\timeSeries.txt';
 [h, allRuns]=hdrload(filename);
 
-maxRuns=100;
+maxRuns=190;
 
 hursts=zeros(maxRuns,1);
-
-hold all;
+inputs=zeros(maxRuns,1);
 
 for run=1:maxRuns
     run
-    data = allRuns(find((allRuns(:,1)==run) & (allRuns(:,2)>2000)),6);
-    hursts(run) = estimate_hurst_exponent(data');
+    data = allRuns(find((allRuns(:,1)==run) & (allRuns(:,2)>2000)),:);
+    hursts(run) = estimate_hurst_exponent(data(:,6)');
+    inputs(run)=mean(data(:,3));
 end
 
-title('ACF for different values of D','FontSize',14);
-xlabel('Lag','FontSize',14);
-ylabel('Correlatoin','FontSize',14);
-legend('1','0.1','0.01','0.001','0.0001','0.00001');
 
-hold off
 
- indicies=find(vec(:,7)==size);
-    fitx=(1+abs(vec(indicies,5)));
-    fity=(1+abs(vec(indicies,6)));
 
-    r=ksr(fitx,fity,1,100);
+points=100;
+spacing=0.01;
 
-    mins=10000000+zeros(length(r.x),1);
-    maxs=zeros(length(r.x),1);
+fitx=inputs;
+fity=hursts;
 
-    for c=1:20
-        th=rand(length(fitx),1);
-        indicies=find(th(:)>0.95);
-        tfitx=fitx(indicies);
-        tfity=fity(indicies);
-        r=ksr(tfitx,tfity,1,100);
-        mins=min(mins,r.f');
-        maxs=max(maxs,r.f');
-    end
+r=ksr(fitx,fity,spacing,points);
 
-    r=ksr(fitx,fity,1,100);
+mins=10000000+zeros(length(r.x),1);
+maxs=zeros(length(r.x),1);
 
-    plot(r.x,r.f,'LineWidth',1,'Color',char(colors(size)));
-    ciplot(mins,maxs,r.x,char(colors(size)));
+for c=1:20
+    th=rand(length(fitx),1);
+    indicies=find(th(:)>0.2);
+    tfitx=fitx(indicies);
+    tfity=fity(indicies);
+    r=ksr(tfitx,tfity,spacing,points);
+    mins=min(mins,r.f');
+    maxs=max(maxs,r.f');
+end
 
-    size
+r=ksr(fitx,fity,spacing,points);
+hold on;
+ciplot(mins,maxs,r.x,'red');
+plot(r.x,r.f,'LineWidth',2,'Color','red');
+hold off;
 
-filename = '..\\graphics\\comparisonOfACFs.pdf';
-orient tall;
-print('-dpdf', filename);
+title('Hurst exponent for different values of s','FontSize',14);
+xlabel('Value of s','FontSize',14);
+ylabel('Hurst exponent','FontSize',14);
+
+alpha(0.5)
+    
+filename = 'comparisonOfHurstsS.bmp';
+orient landscape;
+print('-dbmp', filename);
+
